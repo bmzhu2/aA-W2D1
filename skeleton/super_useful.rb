7593afb46a3f30f43ae8acd1b4f7670ef9f7ce0e
bmzhu2 <brian.m.zhu@gmail.com>
2019-03-18 14:18:36 -0700

    Adds error handling in super_useful.rb

--- skeleton/super_useful.rb
+++ skeleton/super_useful.rb
@@ -1,15 +1,25 @@
 # PHASE 2
 def convert_to_int(str)
-  Integer(str)
+  begin
+    Integer(str)
+  rescue
+    nil
+  end
 end
 
 # PHASE 3
+class CoffeeError < StandardError
+
+end
+
 FRUITS = ["apple", "banana", "orange"]
 
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
+  elsif maybe_fruit == "coffee"
+    raise CoffeeError
+  else
     raise StandardError 
   end 
 end
@@ -18,13 +28,29 @@ def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
   puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  begin
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue CoffeeError => e
+    puts "That's not a fruit but because I'm a friendly monster, I'll let you try again :D"
+    retry
+  rescue 
+    puts "That's not a fruit D:"
+  end
 end  
 
 # PHASE 4
+class NotEnoughYearsError < StandardError
+end
+
+class BlankInputError < StandardError
+end
+
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    raise NotEnoughYearsError if yrs_known < 5
+    raise BlankInputError  if name.length <= 0 || fav_pastime.length <= 0
+
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
